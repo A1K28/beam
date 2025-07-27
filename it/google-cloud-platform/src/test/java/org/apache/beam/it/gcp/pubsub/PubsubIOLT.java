cf1db6902b3b5f0b99121d03888c1bf16235359b
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2025-07-27 15:00:21 +0400

    patch

--- it/google-cloud-platform/src/test/java/org/apache/beam/it/gcp/pubsub/PubsubIOLT.java
+++ it/google-cloud-platform/src/test/java/org/apache/beam/it/gcp/pubsub/PubsubIOLT.java
@@ -234,7 +234,7 @@ public class PubsubIOLT extends IOLoadTestBase {
         do {
           Thread.sleep(5_000);  // 5 seconds between polls
           state = pipelineLauncher.getJobStatus(project, region, readLaunchInfo.jobId());
-        } while (state == PipelineLauncher.JobState.RUNNING);
+        } while (state == PipelineLauncher.JobState.RUNNING || state == PipelineLauncher.JobState.JOB_STATE_DRAINING);
       } catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
