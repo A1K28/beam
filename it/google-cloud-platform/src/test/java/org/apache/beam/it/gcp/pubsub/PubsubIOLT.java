a27357cc9f3aa877327828b59b995857672e10a4
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2025-07-27 05:16:32 +0400

    patch

--- it/google-cloud-platform/src/test/java/org/apache/beam/it/gcp/pubsub/PubsubIOLT.java
+++ it/google-cloud-platform/src/test/java/org/apache/beam/it/gcp/pubsub/PubsubIOLT.java
@@ -155,7 +155,7 @@ public class PubsubIOLT extends IOLoadTestBase {
     }
     writePipeline.getOptions().as(PubsubOptions.class).setProject(project);
     readPipeline.getOptions().as(PubsubOptions.class).setProject(project);
-    // writePipeline.getOptions().as(DirectOptions.class).setBlockOnRun(false);
+    writePipeline.getOptions().as(DirectOptions.class).setBlockOnRun(false);
     readPipeline.getOptions().as(DirectOptions.class).setBlockOnRun(false);
 
     if (configuration.exportMetricsToInfluxDB) {
@@ -214,19 +214,6 @@ public class PubsubIOLT extends IOLoadTestBase {
 
     WriteAndReadFormat format = WriteAndReadFormat.valueOf(configuration.writeAndReadFormat);
     PipelineLauncher.LaunchInfo writeLaunchInfo = testWrite(format);
-
-    // wait for write → RUNNING
-    try {
-        while (pipelineLauncher.getJobStatus(project, region, writeLaunchInfo.jobId()) 
-          != PipelineLauncher.JobState.RUNNING) {
-        Thread.sleep(2_000);
-      }
-      // give Pub/Sub a few seconds to buffer
-      Thread.sleep(10_000);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
-
     PipelineLauncher.LaunchInfo readLaunchInfo = testRead(format);
     PipelineOperator.Result readResult =
         pipelineOperator.waitUntilDone(
