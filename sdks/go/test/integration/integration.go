cfbbe64bc73d2e35466963d2f6c51c1be1ac8141
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2025-07-27 08:57:03 +0400

    patch

--- sdks/go/test/integration/integration.go
+++ sdks/go/test/integration/integration.go
@@ -283,6 +283,16 @@ var sparkFilters = []string{
 }
 
 var dataflowFilters = []string{
+	// Temporary filters
+	// "TestXLang_Prefix",
+	"TestXLang_CoGroupBy",
+	"TestXLang_Combine",
+	"TestXLang_CombineGlobally",
+	"TestXLang_Flatten",
+	"TestXLang_GroupBy",
+	"TestXLang_Multi",
+	"TestXLang_Partition",
+	"TestBigQueryIO_BasicWriteRead",
 	// The Dataflow runner doesn't work with tests using testcontainers locally.
 	"TestJDBCIO_BasicReadWrite",
 	"TestJDBCIO_PostgresReadWrite",
