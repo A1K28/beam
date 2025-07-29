84ce221134c1c91fc4c4624895b46df636c20aff
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2025-07-29 16:53:17 +0400

    awd

--- sdks/java/testing/expansion-service/src/test/java/org/apache/beam/sdk/testing/expansion/TestExpansionService.java
+++ sdks/java/testing/expansion-service/src/test/java/org/apache/beam/sdk/testing/expansion/TestExpansionService.java
@@ -202,11 +202,8 @@ public class TestExpansionService {
       public PTransform<PCollection<? extends String>, PCollection<String>> buildExternal(
           StringConfiguration configuration) {
         return MapElements.into(TypeDescriptors.strings())
-            .via((String x) -> {
-              // Add the debug print line here
-              System.err.println("DATAFOW_DEBUG_TEST: My prefix transform is printing this.");
-              return configuration.data + x;
-            });
+            .via((String x) -> null);
+            // .via((String x) -> configuration.data + x);
       }
     }
 
